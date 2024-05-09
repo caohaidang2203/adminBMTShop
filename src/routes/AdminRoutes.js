4eb275cc7bc916277567a24331e7f6b2174f4e8e
caohaidang2203 <caohaidang2203@gmail.com>
2024-05-10 01:53:39 +0700

    first commit

---
+++ src/routes/AdminRoutes.js
@@ -0,0 +1,22 @@
+import AdminDashboardPage from "../pages/Admin/AdminDashboardPage";
+import AdminAddProductPage from "../pages/Admin/AdminAddProductPage";
+import AdminProductsPage from "../pages/Admin/AdminProductsPage";
+import AdminOrdersPage from "../pages/Admin/AdminOrdersPage";
+import AdminCustomersPage from "../pages/Admin/AdminCustomersPage";
+import AdminAddNewsPage from "../pages/Admin/AdminAddNewsPage";
+import AdminAddBranchPage from "../pages/Admin/AdminAddBranchPage";
+import AdminBranchesPage from "../pages/Admin/AdminBranchesPage";
+import AdminMessagesPage from "../pages/Admin/AdminMessagesPage";
+import AdminOrderDetailPage from "../pages/Admin/AdminOrderDetailPage";
+export {
+  AdminDashboardPage,
+  AdminAddProductPage,
+  AdminProductsPage,
+  AdminOrdersPage,
+  AdminCustomersPage,
+  AdminAddNewsPage,
+  AdminAddBranchPage,
+  AdminBranchesPage,
+  AdminMessagesPage,
+  AdminOrderDetailPage,
+};
